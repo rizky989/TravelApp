f1973360a4021b21a3aa16c8d6f2e8f6929fa702
Rizky Pratama <rizky.pratama98988@gmail.com>
2017-12-09 11:37:59 +0700

    first commit

---
+++ app/src/main/java/com/example/rizkypratama98988/layouting/MainMenu.java
@@ -0,0 +1,13 @@
+package com.example.rizkypratama98988.layouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
